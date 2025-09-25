bfb9e89e3e40577853c65437c58a36f9d10a5166
EfimovSergeiV <efimovsergeiv@gmail.com>
2025-09-25 11:19:40 +0300

    Add:

--- front-end/pages/index.vue
+++ front-end/pages/index.vue
@@ -22,11 +22,11 @@
       <p class="pb-4">table data:</p>
 
 
-        <div class="relative overflow-x-auto mt-8">
+        <div class="relative overflow-y-auto h-[600px] overflow-x-auto mt-8">
           <table class="w-full text-left text-gray-500 dark:text-gray-400">
               <thead class="text-gray-700 uppercase  dark:text-gray-400">
                   <tr class="bg-gray-800 border border-gray-500 hover:bg-gray-600">
-                    <th v-for="key in [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'AA', 'AB', 'AC']" :key="key" scope="col" class="px-6 py-3 text-gray-200 text-xs">
+                    <th v-for="key in Object.keys(data[0])" :key="key" scope="col" class="px-6 py-3 text-gray-200 text-xs">
                       <p class="text-sm">{{ data[0][key] }}</p>
                     </th>
                   </tr>
@@ -34,7 +34,7 @@
               <tbody>
 
                 <tr v-for="item in data.slice(1)" :key="item.id" class=" dark:bg-gray-800 border border-gray-500 hover:bg-gray-600">
-                  <th v-for="value in ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'AA', 'AB', 'AC']" :key="value" scope="row" class="px-6 py-4 text-gray-100 w-[400px] text-xs">
+                  <th v-for="value in  Object.keys(data[0])" :key="value" scope="row" class="px-6 py-4 text-gray-100 w-[400px] text-xs">
                     <p class="text-xs">{{ item[value] }}</p>
                   </th>
                 </tr>
