46d5755a9a7de42263ae6cfda1473f3ed794a937
EfimovSergeiV <efimovsergeiv@gmail.com>
2025-09-25 13:32:07 +0300

    Add:

--- front-end/pages/cht.vue
+++ front-end/pages/cht.vue
@@ -1,24 +1,49 @@
 <script setup>
-import ThreeExample from '~/components/ThreeExample.vue';
+  const config = useRuntimeConfig()
 
 
-
-
+  const data = await $fetch(`${ config.public.baseURL }heroes/`, { method: 'GET' }).catch( () => {
+    return [
+          {
+              "err": "Нет данных",
+          },
+      ]
+  }
+)
 
 </script>
 
 
 <template>
-  
-  
-  <div class="container mx-auto px-4 py-8">
-    <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+  <div class="container mx-auto px-4">
+    
+    <div class="text-gray-100 dark:text-gray-100">
 
-      
+      <p class="pb-4">table data:</p>
+
+
+        <div class="relative overflow-y-auto h-[600px] overflow-x-auto">
+          <table class="w-full text-left text-gray-500 dark:text-gray-400">
+              <thead class="text-gray-700 uppercase dark:text-gray-400 sticky top-0">
+                  <tr class="bg-gray-800 border border-gray-500 hover:bg-gray-600">
+                    <th v-for="key in Object.keys(data[0])" :key="key" scope="col" class="px-6 py-3 text-gray-200 text-xs">
+                      <p class="text-sm">{{ data[0][key] }}</p>
+                    </th>
+                  </tr>
+              </thead>
+              <tbody class="">
+
+                <tr v-for="item in data.slice(1)" :key="item.id" class=" dark:bg-gray-800 border border-gray-500 hover:bg-gray-600">
+                  <th v-for="value in  Object.keys(data[0])" :key="value" scope="row" class="px-6 py-4 text-gray-100 text-xs">
+                    <p class="text-xs  min-w-[150px]">{{ item[value] }}</p>
+                  </th>
+                </tr>
+              </tbody>
+          </table>
+      </div>
+
+
     </div>
 
-  
-  
   </div>
-
 </template>
--- front-end/pages/index.vue
+++ front-end/pages/index.vue
@@ -19,28 +19,10 @@
     
     <div class="text-gray-100 dark:text-gray-100">
 
-      <p class="pb-4">table data:</p>
+      <p class="pb-4">index:</p>
 
 
-        <div class="relative overflow-y-auto h-[600px] overflow-x-auto mt-8">
-          <table class="w-full text-left text-gray-500 dark:text-gray-400">
-              <thead class="text-gray-700 uppercase  dark:text-gray-400">
-                  <tr class="bg-gray-800 border border-gray-500 hover:bg-gray-600">
-                    <th v-for="key in Object.keys(data[0])" :key="key" scope="col" class="px-6 py-3 text-gray-200 text-xs">
-                      <p class="text-sm">{{ data[0][key] }}</p>
-                    </th>
-                  </tr>
-              </thead>
-              <tbody>
 
-                <tr v-for="item in data.slice(1)" :key="item.id" class=" dark:bg-gray-800 border border-gray-500 hover:bg-gray-600">
-                  <th v-for="value in  Object.keys(data[0])" :key="value" scope="row" class="px-6 py-4 text-gray-100 w-[400px] text-xs">
-                    <p class="text-xs">{{ item[value] }}</p>
-                  </th>
-                </tr>
-              </tbody>
-          </table>
-      </div>
 
 
     </div>
