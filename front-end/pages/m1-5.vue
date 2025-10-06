976dd93a0fc11234e1c2a74f00c507b52cf98fae
EfimovSergeiV <efimovsergeiv@gmail.com>
2025-10-06 17:02:36 +0300

    Add:

--- front-end/pages/m1-5.vue
+++ front-end/pages/m1-5.vue
@@ -64,10 +64,12 @@
 
       <div class="grid grid-cols-5 gap-4">
         <div v-for="mm in Object.keys(mms)" :key="mm" class="">
-          
-          <div v-for="(data, pk) in mms[mm]" :key="data" class="">
-            <div class="w-64 text-xs"><p>{{ pk }}. {{ data }}</p></div>
+          <div class="grid grid-cols-1 gap-2">
+            <div v-for="(data, pk) in mms[mm]" :key="data" class="">
+              <div class="w-64 text-xs"><p>{{ pk }}. {{ data }}</p></div>
+            </div>            
           </div>
+
           
         </div>        
       </div>
